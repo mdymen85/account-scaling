cc88f45712452a95fbe13e650021f6abf3e8938a
Martin Dymenstein <martin.dymenstein@thomsonreuters.com>
2022-12-08 21:23:28 -0300

    init

---
+++ src/main/java/com/mdymen/accountscale/AccountScaleApplication.java
@@ -0,0 +1,13 @@
+package com.mdymen.accountscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountScaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdymen/accountscale/AccountScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdymen.accountscale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
